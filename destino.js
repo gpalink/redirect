f45bcdd4b9f73c2b705a31bd86b01fe9dee7c6ca
gpalink <gmoreirax01@gmail.com>
2026-08-23 11:31:53 -0300

    Update destino.js

--- destino.js
+++ destino.js
@@ -1,4 +1,4 @@
 // ============================================
 // TROQUE APENAS A LINHA ABAIXO PARA MUDAR O DESTINO DO QR CODE
 // ============================================
-const DESTINO = "https://www.google.com";
+const DESTINO = "https://www.google.com.br";
