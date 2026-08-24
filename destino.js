44a478a8759401163f3471b3a36bcf3e2a91eb37
gpalink <gmoreirax01@gmail.com>
2026-08-24 09:58:00 -0300

    Update destino.js

--- destino.js
+++ destino.js
@@ -1,4 +1,4 @@
 // ============================================
 // TROQUE APENAS A LINHA ABAIXO PARA MUDAR O DESTINO DO QR CODE
 // ============================================
-const DESTINO = "https://www.ifood.com.br";
+const DESTINO = "https://www.paodeacucar.com.br";
